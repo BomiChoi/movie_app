bcf8ea5b304f06a735bf591b791d2fdfd5a6840b
Bomi Choi <hibomi97@gmail.com>
2020-10-16 23:59:07 +0900

    Updates

---
+++ precache-manifest.8a30c2065f6f64cee8a432182cbc6d44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fba61d784a68f15b81888356b6ba6bf",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "b2c448fa5e8a68fd46bf",
+    "url": "/movie_app/static/css/main.fb9dd8ea.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2c448fa5e8a68fd46bf",
+    "url": "/movie_app/static/js/main.e7bb6a64.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
